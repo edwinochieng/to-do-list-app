76481a3fe355ec4bfa4d2af45bc414c0cbb186b9
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-06 07:37:05 +0300

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from "./components/header"
+import Input from './components/input';
+import ToDoItem from './components/list';
 
 
 function App() {
+   const [ items , setItems] = useState([])
+
+   const addItems = (input) =>{
+      setItems(prevItems =>{
+        return [...prevItems, input]
+      });
+   }
+
   return (
-   <div>Hello</div>
+   <div className='flex flex-col items-center mt-8 mx-96 bg-teal-100 rounded-lg border border-solid border-cyan-300 w-96 h-96 '>
+
+   <div className='py-8'>
+   <Header/>
+   </div>
+    
+    <div>
+    <Input
+      onAdd = {addItems}
+    />
+    </div>
+
+
+    <div className='pt-4'>
+    <ul>
+    {
+      items.map(item =>(
+        <ToDoItem
+         text = {item}
+     />
+     ))}
+    
+    </ul>
+    
+    </div>
+    
+
+   </div>
   );
 }
 
